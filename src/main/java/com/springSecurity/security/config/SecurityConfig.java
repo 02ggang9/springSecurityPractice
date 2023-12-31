8d00c7a8f7da4ae4304203bb40ca48330d3b1fdd
02ggang9 <02ggang9@gmail.com>
2023-12-31 17:30:27 +0900

    feat: 권한 설정과 표현식

--- src/main/java/com/springSecurity/security/config/SecurityConfig.java
+++ src/main/java/com/springSecurity/security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,24 +17,37 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, sys, admin);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.
-                        anyRequest().authenticated()
+        return http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/admin/pay").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SYS")
+                        .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
-                        .addLogoutHandler((request, response, authentication) -> {
-                            HttpSession session = request.getSession();
-                            session.invalidate(); // 세션 무효화
-                        })
-                        .logoutSuccessHandler((request, response, authentication) -> {
-                            response.sendRedirect("/login");
-                        })
-                        .deleteCookies("remember-me")
-                        .permitAll()
-                )
                 .build();
     }
 
